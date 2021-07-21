0fa114bd80a477d5b22ec49b1397775d60002159
Sergey <tttim777@yansex.ru>
2021-07-21 17:53:37 +0300

    php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+ $i = 1;
+/* while ($i <= 10) {
+   echo $i."<br>";
+   $i++;
+ }
+ do {
+  echo $i."<br>";
+  $i++;
+ } while ($i <= 10);
+ for ($i=0; $i <= 10 ; $i++) { 
+  echo $i."<br>";
+ }*/
+ $arr[0] = "product";
+ $arr[1] = "water";
+ $arr[2] = "wiskas";
+ /* for ($i=0; $i < count($arr); $i++) { 
+    echo $arr[$i]."<br>";
+  }*/
+  foreach ($arr as $key => $value) {
+   echo $value."<br>";
+  }
+ 
+ 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
